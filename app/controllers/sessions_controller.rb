8a02116a296dfefc2a555c5dfcd39a129a0e6fd6
Adong520 <A18758@hotmail.com>
2017-06-01 00:06:49 +1000

    Implement advanced login1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
 
   	if user && user.authenticate(params[:session][:password])
   		log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
   		flash[:notice] = 'logged in'
   		
   		redirect_to user
@@ -18,7 +19,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 
